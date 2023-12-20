d88ae45b24b49af80a5df28cdb25cf2eb9b58c78
Arima057nn <tjendung97pain@gmail.com>
2023-12-20 09:14:01 +0700

    add panigation

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -3,55 +3,177 @@ import Bookitem from "../components/bookitem";
 import Category from "../components/category";
 import Search from "../components/search";
 import { bookApi } from "@/app/services";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import Action from "../components/action";
 import { BooksInterface } from "../models/books";
-import { useSearchParams } from "next/navigation";
+import { Pagination } from "@mui/material";
+import { useRouter } from "next/navigation";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}) {
   const [books, setBooks] = useState<any>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState();
+  const router = useRouter();
 
-  const searchParams = useSearchParams();
+  const handlePageChange = (newPage) => {
+    // Thay đổi trang và cập nhật URL
+    setCurrentPage(newPage);
+    console.log("page current:", newPage);
 
+    if (searchParams.title !== undefined) {
+      if (searchParams.page === undefined) {
+        router.push(`/?title=${searchParams.title}`);
+      } else {
+        router.push(`/?title=${searchParams.title}&page=${searchParams.page}`);
+      }
+    } else if (searchParams.author !== undefined) {
+      if (searchParams.page === undefined) {
+        router.push(`/?author=${searchParams.author}`);
+      } else {
+        router.push(
+          `/?author=${searchParams.author}&page=${searchParams.page}`
+        );
+      }
+    } else if (searchParams.publishingTime !== undefined) {
+      if (searchParams.page === undefined) {
+        router.push(`/?publishingTime=${searchParams.publishingTime}`);
+      } else {
+        router.push(
+          `/?publishingTime=${searchParams.publishingTime}&page=${searchParams.page}`
+        );
+      }
+    } else if (searchParams.library !== undefined) {
+      if (searchParams.page === undefined) {
+        router.push(`/?library=${searchParams.library}`);
+      } else {
+        router.push(
+          `/?library=${searchParams.library}&page=${searchParams.page}`
+        );
+      }
+    } else if (searchParams.categoryId !== undefined) {
+      if (searchParams.page === undefined) {
+        router.push(`/?categoryId=${searchParams.categoryId}`);
+      } else {
+        router.push(
+          `/?categoryId=${searchParams.categoryId}&page=${searchParams.page}`
+        );
+      }
+    } else {
+      router.push(`/?page=${newPage}`);
+    }
+  };
   useEffect(() => {
-    if (searchParams.get("title") !== null) getBooks(searchParams.get("title"));
-    else if (searchParams.get("author") !== null)
-      getBooks(searchParams.get("author"));
-    else if (searchParams.get("publishingTime") !== null)
-      getBooks(searchParams.get("publishingTime"));
-    else if (searchParams.get("library") !== null)
-      getBooks(searchParams.get("library"));
-    else if (searchParams.get("categoryId") !== null)
-      getBooks(searchParams.get("categoryId"));
+    if (searchParams.title !== undefined) getBooks(searchParams.title);
+    else if (searchParams.author !== undefined) getBooks(searchParams.author);
+    else if (searchParams.publishingTime !== undefined)
+      getBooks(searchParams.publishingTime);
+    else if (searchParams.library !== undefined) getBooks(searchParams.library);
+    else if (searchParams.categoryId !== undefined)
+      getBooks(searchParams.categoryId);
     else {
-      getBooks(searchParams.get("title"));
+      getBooks(searchParams.title);
     }
   }, [searchParams]);
 
   const getBooks = async (search: any) => {
     console.log("search", search);
     let res;
-    if (search === null) {
+    if (search === undefined) {
       search = "";
-      res = await bookApi.searchBook(search, "", "", "", "");
+      if (searchParams.page === undefined) {
+        res = await bookApi.searchBook(search, "", "", "", "", "1");
+      } else {
+        res = await bookApi.searchBook(
+          search,
+          "",
+          "",
+          "",
+          "",
+          searchParams.page
+        );
+      }
     } else {
-      if (searchParams.get("title") !== null) {
-        res = await bookApi.searchBook(search, "", "", "", "");
+      if (searchParams.title !== undefined) {
+        if (searchParams.page === undefined) {
+          res = await bookApi.searchBook(search, "", "", "", "", "1");
+        } else {
+          res = await bookApi.searchBook(
+            search,
+            "",
+            "",
+            "",
+            "",
+            searchParams.page
+          );
+        }
       }
-      if (searchParams.get("author") !== null) {
-        res = await bookApi.searchBook("", search, "", "", "");
+      if (searchParams.author !== undefined) {
+        if (searchParams.page === undefined) {
+          res = await bookApi.searchBook("", search, "", "", "", "1");
+        } else {
+          res = await bookApi.searchBook(
+            "",
+            search,
+            "",
+            "",
+            "",
+            searchParams.page
+          );
+        }
       }
-      if (searchParams.get("publishingTime") !== null) {
-        res = await bookApi.searchBook("", "", search, "", "");
+      if (searchParams.publishingTime !== undefined) {
+        if (searchParams.page === undefined) {
+          res = await bookApi.searchBook("", "", search, "", "", "1");
+        } else {
+          res = await bookApi.searchBook(
+            "",
+            "",
+            search,
+            "",
+            "",
+            searchParams.page
+          );
+        }
       }
-      if (searchParams.get("library") !== null) {
-        res = await bookApi.searchBook("", "", "", search, "");
+      if (searchParams.library !== undefined) {
+        if (searchParams.page === undefined) {
+          res = await bookApi.searchBook("", "", "", search, "", "1");
+        } else {
+          res = await bookApi.searchBook(
+            "",
+            "",
+            "",
+            search,
+            "",
+            searchParams.page
+          );
+        }
       }
-      if (searchParams.get("categoryId") !== null) {
-        res = await bookApi.searchBook("", "", "", "", search);
+      if (searchParams.categoryId !== undefined) {
+        if (searchParams.page === undefined) {
+          res = await bookApi.searchBook("", "", "", "", search, "1");
+        } else {
+          res = await bookApi.searchBook(
+            "",
+            "",
+            "",
+            "",
+            search,
+            searchParams.page
+          );
+        }
       }
     }
-    if (res !== undefined) setBooks(res.data?.content);
+    if (res !== undefined) {
+      setBooks(res.data?.content);
+      setTotalPages(res.data?.totalPages);
+    }
   };
   return (
     <div>
@@ -69,6 +191,13 @@ export default function Home() {
       {books?.map((book: BooksInterface) => (
         <Bookitem book={book} />
       ))}
+      <div className="flex justify-center m-4">
+        <Pagination
+          count={totalPages}
+          onChange={(event, page) => handlePageChange(page)}
+          color="primary"
+        />
+      </div>
     </div>
   );
 }
